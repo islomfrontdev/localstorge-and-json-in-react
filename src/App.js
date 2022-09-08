68e8123a1e689ca4de772186c83d4b8f73c8ed72
islomfrontdev <94689541+islomfrontdev@users.noreply.github.com>
2022-09-09 00:35:17 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [item, setItem] = useState("");
+  const persone = {
+    name: "islom",
+    age: "23",
+  };
+  const addItem = () => {
+    localStorage.setItem("name", item);
+    localStorage.setItem("person", JSON.stringify(persone));
+  };
+  const deleteItem = () => {
+    localStorage.removeItem("name");
+  };
+  const clearItems = () => {
+    localStorage.clear();
+  };
+  const personInfo = JSON.parse(localStorage.getItem("person"));
+  personInfo.name = "bobur";
+  console.log(personInfo);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>LocaStorage</h1>
+      <input
+        type={"text"}
+        placeholder="Enter your name"
+        onChange={(e) => setItem(e.target.value)}
+        value={item}
+      />
+      <button onClick={addItem}>Add Item</button>
+      <button onClick={deleteItem}>Delete Item</button>
+      <button onClick={clearItems}>Delete All Items</button>
+      <p>{localStorage.getItem("name")}</p>
     </div>
   );
 }
